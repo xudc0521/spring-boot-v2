eaf29abf53cdd3e2f1b822d5dd1c522ee21776f2
xudc <xudc0521@hotmail.com>
2019-04-18 17:07:52 +0800

    Init Commit

--- spring-boot-v2-webflux/src/main/java/com/xudc/webflux/SpringBootWebfluxApplication.java
+++ spring-boot-v2-webflux/src/main/java/com/xudc/webflux/SpringBootWebfluxApplication.java
@@ -4,12 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
 
+/**
+ * @author xudc
+ */
 @SpringBootApplication
 @EnableReactiveMongoRepositories
-public class SpringBootV2WebfluxApplication {
+public class SpringBootWebfluxApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringBootV2WebfluxApplication.class, args);
+        SpringApplication.run(SpringBootWebfluxApplication.class, args);
     }
 
 }
